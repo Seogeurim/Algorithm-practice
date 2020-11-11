1c98e84290b8d4a86824307c263d4b9d86c5fa75
Seogeurim <geulims@naver.com>
2020-11-11 18:46:10 +0900

    [DP] baekjoon-11727

---
+++ src/DP/P11727/Main.java
@@ -0,0 +1,22 @@
+package DP.P11727;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int n;
+    static int[] dp = new int[1001];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        dp[1] = 1;
+        dp[2] = 3;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
