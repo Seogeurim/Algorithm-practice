3435b6be6f12cf1698bf031d3f6953a77765d433
Seogeurim <geulims@naver.com>
2021-02-04 21:19:58 +0900

    [DP] baekjoon-15990 update alg

--- src/DP/P15990/Main.java
+++ src/DP/P15990/Main.java
@@ -11,20 +11,27 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("src/DP/P15990/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         T = Integer.parseInt(br.readLine());
 
         dp[1][1] = 1; dp[1][2] = 0; dp[1][3] = 0;
         dp[2][1] = 0; dp[2][2] = 1; dp[2][3] = 0;
         dp[3][1] = 1; dp[3][2] = 1; dp[3][3] = 1;
+        for (int i = 4; i <= 100000; i++) {
+            dp[i][1] = (dp[i-1][2] + dp[i-1][3]) % mod;
+            dp[i][2] = (dp[i-2][1] + dp[i-2][3]) % mod;
+            dp[i][3] = (dp[i-3][1] + dp[i-3][2]) % mod;
+        }
 
+        StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
             n = Integer.parseInt(br.readLine());
-            for (int i = 4; i <= n; i++) {
-                dp[i][1] = (dp[i-1][2] + dp[i-1][3]) % mod;
-                dp[i][2] = (dp[i-2][1] + dp[i-2][3]) % mod;
-                dp[i][3] = (dp[i-3][1] + dp[i-3][2]) % mod;
-            }
-            System.out.println((dp[n][1] + dp[n][2] + dp[n][3]) % mod);
+            sb.append((dp[n][1] + dp[n][2] + dp[n][3]) % mod).append("\n");
         }
+        bw.write(sb.toString());
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
--- src/DP/P15990/README.md
+++ src/DP/P15990/README.md
@@ -3,3 +3,9 @@
 ![image](https://user-images.githubusercontent.com/22045163/99809431-9e374880-2b85-11eb-8108-a22ebe22a9b0.png)
 
 ![IMG_3E4C86D36612-1](https://user-images.githubusercontent.com/22045163/99809685-fec68580-2b85-11eb-8791-f6e168364e23.jpeg)
+
+### 2021.02.04 - 재채점 및 시간초과로 인해 다시 풀이
+
+테스트케이스가 있으므로 dp 배열은 미리 초기화, StringBuilder 사용
+
+![image](https://user-images.githubusercontent.com/22045163/106891625-5197ee00-672e-11eb-95cd-3e0f3ddda779.png)
