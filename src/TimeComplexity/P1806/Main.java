ed5d18966248ca784e4c9ccbc1eede17e340a678
Seogeurim <geulims@naver.com>
2021-02-07 01:38:56 +0900

    [TimeComplexity] baekjoon-1806 update logic because of time complexity

--- src/TimeComplexity/P1806/Main.java
+++ src/TimeComplexity/P1806/Main.java
@@ -1,9 +1,7 @@
 package TimeComplexity.P1806;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -12,29 +10,28 @@ public class Main {
     static int ans = 0;
 
     public static void main(String[] args) throws Exception {
-//        System.setIn(new FileInputStream("src/TimeComplexity/P1806/input.txt"));
-
+        System.setIn(new FileInputStream("src/TimeComplexity/P1806/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
 
-        nums = new int[N + 1];
+        nums = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
             if (nums[i] == S) ans = 1;
+            if (i > 0) nums[i] += nums[i-1];
         }
 
-        if (ans == 0) {
+        if (ans != 1) {
             int low = 0;
             int high = 1;
             while (high < N && low < high) {
-                long sum = 0;
-                for (int i = low; i <= high; i++) {
-                    sum += nums[i];
-                }
+                long sum;
+                if (low == 0) sum = nums[high];
+                else sum = nums[high] - nums[low-1];
                 if (sum >= S) {
                     if (ans == 0) ans = high - low + 1;
                     else ans = Math.min(ans, high - low + 1);
---
+++ src/TimeComplexity/P1806/README.md
@@ -0,0 +1,5 @@
+## [baekjoon-1806] 부분합
+
+![image](https://user-images.githubusercontent.com/22045163/107124083-01af5780-68e5-11eb-9511-aae420c76c28.png)
+
+![image](https://user-images.githubusercontent.com/22045163/107124091-0aa02900-68e5-11eb-890e-5ec10681feb6.png)
