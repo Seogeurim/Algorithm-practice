9e74186c6f37fc4e7614e409a28ec8e793d31575
Seogeurim <geulims@naver.com>
2021-04-13 01:25:43 +0900

    [BFS] swea-1249 new solution

--- src/BFS/swea1249/README.md
+++ src/BFS/swea1249/README.md
@@ -4,4 +4,11 @@
 ![image](https://user-images.githubusercontent.com/22045163/114415067-52477780-9bea-11eb-9e30-c347858905a8.png)
 ![image](https://user-images.githubusercontent.com/22045163/114415149-625f5700-9bea-11eb-8ecb-a3ea63b1fd5d.png)
 
-![image](https://user-images.githubusercontent.com/22045163/114415206-6f7c4600-9bea-11eb-883d-9a961c76b682.png)
+### 풀이 과정
+
+문제가 장황해보이지만 시작 위치에서 도착 위치까지 가는 경로 중 최소 비용을 구하는 문제이다. 그리고 이에 대해 너무 어렵게 생각하지 않았을 때 좋은 로직을 구할 수 있다. 이 문제에는 다음의 두 가지 솔루션이 가장 좋은 것 같다.
+
+- [BFS + DP](Solution.java)
+  ![image](https://user-images.githubusercontent.com/22045163/114427928-8de83e80-9bf6-11eb-9170-0aa99597c1b6.png)
+- [BFS + Priority Queue](Solution2.java)
+  ![image](https://user-images.githubusercontent.com/22045163/114427871-7c9f3200-9bf6-11eb-9f77-4d6919ab87f9.png)
--- src/BFS/swea1249/Solution2.java
+++ src/BFS/swea1249/Solution2.java
@@ -3,10 +3,10 @@ package BFS.swea1249;
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class Solution2 {
 
     static int T, N;
-    static int[][] map, dp;
+    static int[][] map;
     static int[] di = {-1, 0, 1, 0};
     static int[] dj = {0, 1, 0, -1};
 
@@ -19,50 +19,56 @@ public class Solution {
         for (int t = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
             map = new int[N][N];
-            dp = new int[N][N];
             for (int i = 0; i < N; i++) {
                 String line = br.readLine();
-                for (int j = 0; j < N; j++) {
-                    map[i][j] = line.charAt(j) - '0';
-                    dp[i][j] = Integer.MAX_VALUE;
-                }
+                for (int j = 0; j < N; j++) map[i][j] = line.charAt(j) - '0';
             }
-
-            bfs();
-            sb.append("#").append(t).append(" ").append(dp[N-1][N-1]).append("\n");
+            sb.append("#").append(t).append(" ").append(bfs()).append("\n");
         }
 
         System.out.println(sb.toString());
     }
 
-    static void bfs() {
-        Queue<Pos> q = new LinkedList<>();
-        q.offer(new Pos(0, 0));
-        dp[0][0] = 0;
-        while (!q.isEmpty()) {
-            Pos p = q.poll();
+    static int bfs() {
+        PriorityQueue<Pos> pq = new PriorityQueue<>();
+        boolean[][] visited = new boolean[N][N];
+
+        pq.offer(new Pos(0, 0, 0));
+        visited[0][0] = true;
+        while (!pq.isEmpty()) {
+            Pos p = pq.poll();
+            if (p.i == N-1 && p.j == N-1) return p.d;
+
             for (int k = 0; k < 4; k++) {
                 int to_i = p.i + di[k];
                 int to_j = p.j + dj[k];
 
-                if (isValidPath(to_i, to_j) && dp[to_i][to_j] > dp[p.i][p.j] + map[to_i][to_j]) {
-                    dp[to_i][to_j] = dp[p.i][p.j] + map[to_i][to_j];
-                    q.offer(new Pos(to_i, to_j));
+                if (isValidPath(to_i, to_j) && !visited[to_i][to_j]) {
+                    visited[to_i][to_j] = true;
+                    pq.offer(new Pos(to_i, to_j, p.d + map[to_i][to_j]));
                 }
             }
         }
+
+        return 0;
     }
 
     static boolean isValidPath(int i, int j) {
         return 0 <= i && i < N && 0 <= j && j < N;
     }
 
-    static class Pos {
-        int i, j;
+    static class Pos implements Comparable<Pos> {
+        int i, j, d;
 
-        public Pos(int i, int j) {
+        public Pos(int i, int j, int d) {
             this.i = i;
             this.j = j;
+            this.d = d;
+        }
+
+        @Override
+        public int compareTo(Pos o) {
+            return d - o.d;
         }
     }
 }
